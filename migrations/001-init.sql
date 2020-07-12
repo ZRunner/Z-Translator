f50ee86e2830c059ba0de75fc2a88f0abc5a2ac5
ZRunner <z.runner.mc@gmail.com>
2020-07-12 12:47:12 +0200

    move projects to sqlite database

--- migrations/001-init.sql
+++ migrations/001-init.sql
@@ -4,7 +4,7 @@ CREATE TABLE `users` (
   `nickname` VARCHAR(50) NOT NULL,
   `password` TEXT NOT NULL,
   `email` TEXT NOT NULL,
-  `avatar-url`TEXT,
+  `avatar-url` TEXT,
   `minecraft-name` TEXT,
   `minecraft-uuid` TEXT UNIQUE,
   `discord-id` BIGINT UNIQUE,
@@ -12,6 +12,20 @@ CREATE TABLE `users` (
 );
 CREATE UNIQUE INDEX idx_accounts_email ON `users` (`email`);
 CREATE INDEX idx_accounts_nickname ON `users` (`nickname`);
+
+CREATE TABLE `projects` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` TEXT,
+  `owner` BIGINT,
+  `git-url` TEXT NOT NULL,
+  `git-path` TEXT NOT NULL,
+  `last-update` TIMESTAMP,
+  `last-commit` TEXT,
+  `settings-path` TEXT
+);
+CREATE UNIQUE INDEX idx_accounts_email ON `projects` (`name`);
+CREATE INDEX idx_accounts_nickname ON `projects` (`owner`);
  
 -- Down 
 DROP TABLE IF EXISTS `users`;
