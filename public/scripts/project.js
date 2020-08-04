4fe6e6b45f6c85ad4e3864c18f83eb7d543c06e1
Arthur <arthur@MacBook-Pro-de-Cyril-2.local>
2020-08-04 18:59:23 +0200

    closed WS + translation panel beginning

--- public/scripts/project.js
+++ public/scripts/project.js
@@ -1,7 +1,6 @@
 let new_uri = (window.location.protocol === "https:") ? "wss:" : "ws:";
 new_uri += `//${window.location.host}${window.location.pathname.replace("/project/", "/project-ws/")}`
 var Data = {};
-var projectSocket;
 
 function msg(message, data) {
     if (data === undefined || data === null) {
@@ -10,30 +9,45 @@ function msg(message, data) {
     return JSON.stringify({ message: message, data: data });
 }
 
-function select_lang(lang) {
-    projectSocket.current_lang = lang;
+function display_section(id) {
     $(".section").addClass('d-none');
-    $("#loading").removeClass('d-none');
+    $('#'+id).removeClass('d-none');
+    if (id == "tr-panel")
+        $('#mainzone').addClass('full');
+    else
+        $('#mainzone').removeClass('full');
+}
+
+function select_lang(lang) {
+    Data.current_lang = lang;
+    display_section('loading');
+    Data.ws.send(msg("load-language", { "language": lang }))
 }
 
 function main() {
-    projectSocket = new WebSocket(new_uri);
+    display_section('loading');
+    Data.ws = new WebSocket(new_uri);
 
-    projectSocket.addEventListener('open', ev => {
-        console.debug("WS: opened", ev);
+    Data.ws.addEventListener('open', ev => {
+        console.log("WS: opened", ev);
     })
 
-    projectSocket.addEventListener('message', ev => {
+    Data.ws.addEventListener('close', ev => {
+        console.warn("WS: closed by server:",ev);
+        display_section('closed');
+    })
+
+    Data.ws.addEventListener('message', ev => {
         try {
             var body = JSON.parse(ev.data);
         } catch {
             console.warn("WS: not json message:", ev.data);
             return;
         }
-        console.debug("WS: new message", body);
+        console.log("WS: new message", body);
 
         // First connection
-        if (body.message = "connected") {
+        if (body.message == "connected") {
             Data.languages = body.data.languages;
             Data.project = body.data.project;
             $("#langs-btn").empty();
@@ -42,8 +56,12 @@ function main() {
                     $("#langs-btn").append(`<button type="button" class="btn btn-secondary" onclick="select_lang('${lang}')">${Data.languages[lang]}</button>`)
                 }
             }
-            $(".section").addClass('d-none');
-            $("#select-lang").removeClass('d-none');
+            display_section('select-lang');
+        } else if (body.message == "load-language") {
+            display_section('tr-panel');
+        }
+        else {
+            console.log("WS: unhandled message")
         }
     })
 }
