7d9cf08a1816cebd3c3dfda0fe7826dcacc9634f
ZRunner <z.runner.mc@gmail.com>
2020-08-05 14:55:30 +0200

    more info on translation panel

--- public/scripts/project.js
+++ public/scripts/project.js
@@ -15,7 +15,7 @@ function isNullOrUndefined(arg) {
 
 function display_section(id) {
     $(".section").addClass('d-none');
-    $('#'+id).removeClass('d-none');
+    $('#' + id).removeClass('d-none');
     if (id == "tr-panel")
         $('#mainzone').addClass('full');
     else
@@ -34,11 +34,21 @@ window.addEventListener('popstate', function (event) {
 });
 
 function display_hash(hash) {
+    // find translation object
+    let obj = Data.translations.filter(e => e.key == hash.substring(1));
+    if (obj.length === 0) return;
+    obj = obj[0];
+    if ($(`a.tr-string[href$="${hash}"]`).length == 0) return;
+    // edit "selected" case
     $(`a.tr-string.selected`).removeClass("selected");
     $(`a.tr-string[href$="${hash}"]`).addClass("selected");
+    // scroll to selected
     $('#tr-left').animate({
-        scrollTop: $("a.tr-string.selected").position().top+$("#tr-left").scrollTop()-20
+        scrollTop: $("a.tr-string.selected").position().top + $("#tr-left").scrollTop() - 20
     }, 800);
+    // display translations
+    $("#tr-origin-txt").text(obj.origin);
+    $("#tr-translated-txt").text(obj.translation);
 }
 
 
@@ -53,7 +63,7 @@ function main() {
     })
 
     Data.ws.addEventListener('close', ev => {
-        console.warn("WS: closed by server:",ev);
+        console.warn("WS: closed by server:", ev);
         display_section('closed');
     })
 
@@ -77,8 +87,11 @@ function main() {
                 }
             }
             display_section('select-lang');
-        } else if (body.message == "load-language") {
+            $("#tr-origin>h5>span").text(Data.languages[Data.project['origin-lang']]);
+        } // starting translation for a language
+        else if (body.message == "load-language") {
             let done = 0;
+            // sort by done/not done
             body.data.translations.sort((a, b) => {
                 un_a = isNullOrUndefined(a.translation);
                 un_b = isNullOrUndefined(b.translation);
@@ -88,15 +101,22 @@ function main() {
                     return -1
                 } if (un_b)
                     return 1;
-            })
+            });
+            Data.translations = body.data.translations;
+            // delete precedent children
+            $('#tr-left').find('*').not('#tr-left-sum').remove();
+            // add new children
             body.data.translations.forEach(tr => {
                 const v = !isNullOrUndefined(tr.translation);
                 $('#tr-left').append(`<a class="tr-string ${v ? "valid" : ""}" href="#${tr.key}">${tr.origin}</a>`)
                 if (v) done += 1;
             });
-            const percent = Math.floor(done/body.data.translations.length*100);
+            // display % done
+            const percent = Math.floor(done / body.data.translations.length * 100);
             $("#tr-left-sum").html(`<span>${Data.languages[body.data.language]}</span> <span>${percent}%</span>`)
             display_section('tr-panel');
+            // display current language
+            $("#tr-area>h5>span").text(Data.languages[body.data.language]);
             if (window.location.hash) display_hash(window.location.hash);
         }
         else {
