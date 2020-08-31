5d41b0d45b5ef24190ae8797c943359d32024928
ZRunner <z.runner.mc@gmail.com>
2020-08-31 14:13:39 +0200

    protect database

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.get("/admin", function (req, res) {
         res.redirect("signin");
         return;
     }
-    res.render("admin", { account: req.session.account, version: VERSION, level: 0 });
+    res.render("admin", { account: req.session.account, version: VERSION, dburl: credentials["db_website"], level: 0 });
 })
 
 app.post("/admin/reboot", function (req, res) {
